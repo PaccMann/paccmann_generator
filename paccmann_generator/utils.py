088dce9a86f2b88be91fe506822ae5ebd58ef249
Jannis Born <jab@zurich.ibm.com>
2020-04-01 18:16:43 +0200

    wip: refactoring for protein driven design

--- paccmann_generator/utils.py
+++ paccmann_generator/utils.py
@@ -51,6 +51,10 @@ def omics_data_splitter(omics_df, site, test_fraction):
         omics_df    A pandas df of omics data for cancer cell lines
         site        The cancer site against which the generator is finetuned
         test_fraction  The fraction of cell lines in test data
+
+    Returns:
+        train_cell_lines, test_cell_lines (tuple): A tuple of lists with the
+            cell line names used for training and testing
     """
     cell_lines = np.array(
         list(set(omics_df[omics_df['site'] == site]['cell_line']))
@@ -60,7 +64,7 @@ def omics_data_splitter(omics_df, site, test_fraction):
     test_cell_lines = cell_lines[inds[:ceil(len(cell_lines) * test_fraction)]]
     train_cell_lines = cell_lines[inds[ceil(len(cell_lines) * test_fraction):]]
 
-    return train_cell_lines, test_cell_lines
+    return train_cell_lines.tolist(), test_cell_lines.tolist()
 
 
 def disable_rdkit_logging():
