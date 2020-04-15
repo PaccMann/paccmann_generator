afc3b5b41afdbec576f3f37bc13df934091be42d
Jannis Born <jab@zurich.ibm.com>
2020-04-15 21:59:43 +0200

    feat: Added support for toxicitiy drug evaluators

---
+++ paccmann_generator/drug_evaluators/tests/test_clintox.py
@@ -0,0 +1,26 @@
+"""Testing ClinTox model."""
+import unittest
+import torch
+import numpy as np
+import os
+from paccmann_generator.drug_evaluators import ClinTox
+
+
+class TestClinTox(unittest.TestCase):
+    """Testing OrganDB model """
+
+    def test_set_reward_fn(self) -> None:
+        """Test set_reward_fn."""
+
+        path = os.path.join(
+            os.path.expanduser('~'), 'Box', 'Molecular_SysBio', 'data',
+            'cytotoxicity', 'models', 'ClinToxMulti'
+        )
+
+        for reward in ['thresholded', 'raw']:
+            model = ClinTox(path, reward)
+            self.assertGreaterEqual(model('CCO'), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
