9391555b54f2fb6c284ed1e5c913042a40216154
Patricia Stoll <tol@zurich.ibm.com>
2020-09-08 17:46:49 +0200

    combined reinforce_omics and reinforce_proteins latent space with average.

---
+++ paccmann_generator/reinforce_proteins_omics.py
@@ -0,0 +1,464 @@
+"""PaccMann^RL: Protein-driven drug generation"""
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import torch
+import torch.nn.functional as F
+
+from pytoda.transforms import LeftPadding, ToTensor
+
+from .reinforce import Reinforce
+
+
+class ReinforceProteinOmics(Reinforce):
+
+    def __init__(
+        self, generator, encoderProtein, encoderOmics, predictorProtein, predictorOmics, 
+        protein_df, gep_df, params, model_name, logger
+    ):
+        """
+        Constructor for the Reinforcement object.
+
+        Args:
+            generator (nn.Module): SMILES generator object.
+            encoderProtein (nn.Module): A protein encoder (DenseEncoder object)
+            encoderOmics (nn.Module): A gene expression encoder (DenseEncoder object)
+            predictorProtein (nn.Module): A binding affinity predictor
+            predictorOmics: A IC50 predictor, i.e. PaccMann (MCA object).
+            protein_df (pd.Dataframe): Protein sequences of interest.
+            gep_df: A pandas df with gene expression profiles of cancer cells.
+                GEP values need to be ordered, s.t. both PaccMann and encoder
+                understand it.
+            params: dict with hyperparameter.
+            model_name: name of the model.
+            logger: a logger.
+
+        Returns:
+            object of type Reinforcement used for biasing the properties
+            estimated by the predictor of trajectories produced by the
+            generator to maximize the custom reward function get_reward.
+        """
+
+        super(ReinforceProteinOmics, self).__init__(
+            generator, encoderProtein, params, model_name, logger
+        )  # yapf: disable
+
+        self.predictorProtein = predictorProtein
+        self.predictorProtein.eval()
+
+        self.encoderOmics = encoderOmics
+        self.encoderOmics.eval()
+
+        self.predictorOmics = predictorOmics
+        self.predictorOmics.eval()
+
+        self.protein_df = protein_df
+
+        self.gep_df = gep_df
+        self.update_params(params)
+
+        self.pad_smiles_predictor = LeftPadding(
+            predictor.smiles_padding_length,
+            predictor.smiles_language.padding_index
+        )
+        self.pad_protein_predictor = LeftPadding(
+            predictor.protein_padding_length,
+            predictor.protein_language.padding_index
+        )
+
+        self.protein_to_tensor = ToTensor(self.device)
+        self.update_params(params)
+
+        self.tox21 = Tox21(
+            params.get(
+                'tox21_path',
+                os.path.join(
+                    os.path.expanduser('~'), 'Box', 'Molecular_SysBio', 'data',
+                    'cytotoxicity', 'models', 'Tox21_deepchem'
+                )
+            )
+        )
+
+    def update_params(self, params):
+        """Update parameter
+
+        Args:
+            params (dict): Dict with (new) parameters
+        """
+        super().update_params(params)
+
+         # critic: upper and lower bound of log(IC50) for de-normalization
+        self.ic50_min = params.get('IC50_min', -8.77435)
+        self.ic50_max = params.get('IC50_max', 11.83146)
+        # efficacy threshold: log(X mol) = thresh. thresh=0 -> 1 micromolar
+        self.ic50_threshold = params.get('IC50_threshold', 0.0)
+        # rewards for efficient and all other valid molecules
+        self.rewards = params.get('rewards', (11, 1))
+
+    def get_log_molar(self, y):
+        """
+        Converts PaccMann predictions from [0,1] to log(micromolar) range.
+        """
+        return y * (self.ic50_max - self.ic50_min) + self.ic50_min
+
+    def encode_cell_line(self, cell_line=None, batch_size=128):
+        """
+        Encodes cell line in latent space with GEP encoder.
+        """
+        gep_t = torch.unsqueeze(
+            torch.Tensor(
+                self.gep_df[
+                    self.gep_df['cell_line'] == cell_line  # yapf: disable
+                ].iloc[0].gene_expression.values
+            ),
+            0
+        )
+        cell_mu, cell_logvar = self.encoderOmics(gep_t)
+
+        latent_z = torch.unsqueeze(
+            self.reparameterize(
+                cell_mu.repeat(batch_size, 1),
+                cell_logvar.repeat(batch_size, 1)
+            ), 0
+        )
+        return latent_z
+
+    def encode_protein(self, protein=None, batch_size=128):
+        """
+        Encodes protein in latent space with protein encoder.
+        Args:
+            protein (str): Name of a protein
+            batch_size (int): batch_size
+        """
+        if protein is None:
+            latent_z = torch.randn(1, batch_size, self.encoder.latent_size)
+        else:
+
+            protein_tensor, _ = self.protein_to_numerical(
+                protein, encoder_uses_sequence=False
+            )
+            protein_mu, protein_logvar = self.encoder(protein_tensor)
+
+            latent_z = torch.unsqueeze(
+                self.reparameterize(
+                    protein_mu.repeat(batch_size, 1),
+                    protein_logvar.repeat(batch_size, 1)
+                ), 0
+            )
+        return latent_z
+
+    def protein_to_numerical(
+        self,
+        protein,
+        encoder_uses_sequence=True,
+        predictor_uses_sequence=True
+    ):
+        """
+        Receives a name of a protein.
+        Returns two numerical torch Tensor, the first for the protein encoder,
+        the second for the affinity predictor.
+        Args:
+            protein (str): Name of the protein
+            encoder_uses_sequence (bool): Whether the encoder uses the protein
+                sequence or an embedding.
+            predictor_uses_sequence (bool): Whether the predictor uses the
+                protein sequence or an embedding.
+
+        """
+
+        if encoder_uses_sequence or predictor_uses_sequence:
+            protein_sequence = self.protein_df.loc[protein]['Sequence']
+            # TODO: This may cause bugs if encoder_uses_sequence is True and
+            # uses another protein language object
+            sequence_tensor = torch.unsqueeze(
+                self.protein_to_tensor(
+                    self.pad_protein_predictor(
+                        self.predictorProtein.protein_language.
+                        sequence_to_token_indexes(protein_sequence)
+                    )
+                ), 0
+            )
+        if (not encoder_uses_sequence) or (not predictor_uses_sequence):
+            # Column names of DF
+            locations = [str(x) for x in range(768)]
+            protein_encoding = self.protein_df.loc[protein][locations]
+            encoding_tensor = torch.unsqueeze(
+                torch.Tensor(protein_encoding), 0
+            )
+        t1 = sequence_tensor if encoder_uses_sequence else encoding_tensor
+        t2 = sequence_tensor if predictor_uses_sequence else encoding_tensor
+        return t1, t2
+
+    def generate_compounds_and_evaluate(
+        self,
+        epoch,
+        batch_size,
+        protein=None,
+        cell_line=None,
+        primed_drug=' ',
+        return_latent=False,
+        remove_invalid=True
+    ):
+        """
+        Generate some compounds and evaluate them with the predictor
+
+        Args:
+            epoch (int): The training epoch.
+            batch_size (int): The batch size.
+            protein (str): A string, the protein used to drive generator.
+            cell_line (str): A string, the cell_line used to drive generator.
+            primed_drug (str): SMILES string to prime the generator.
+
+        Returns:
+            np.array: Predictions from PaccMann.
+        """
+        if primed_drug != ' ':
+            raise ValueError('Drug priming not yet supported.')
+
+        self.predictorProtein.eval()
+        self.predictorOmics.eval()
+        self.encoder.eval()
+        self.encoderOmics.eval()
+        self.generator.eval()
+
+        if protein is None:
+            # Generate a random molecule
+            latent_z = torch.randn(
+                1, batch_size, self.generator.decoder.latent_dim
+            )
+        else if cell_line is None:
+            # Generate a random molecule
+            latent_z = torch.randn(
+                1, batch_size, self.generator.decoder.latent_dim
+            )
+        else:
+            protein_encoder_tensor, protein_predictor_tensor = (
+                self.protein_to_numerical(
+                    protein, encoder_uses_sequence=False
+                )
+            )
+            protein_mu, protein_logvar = self.encoder(protein_encoder_tensor)
+
+            gep_t = torch.unsqueeze(
+                torch.Tensor(
+                    self.gep_df[
+                        self.gep_df['cell_line'] == cell_line  # yapf: disable
+                    ].iloc[0].gene_expression.values
+                ),
+                0
+            )
+            cell_mu, cell_logvar = self.encoderOmics(gep_t)
+
+            # TODO: I need to make sure that I only sample around the encoding
+            # of the protein, not the entire latent space.
+            latent_z_protein = torch.unsqueeze(
+                self.reparameterize(
+                    protein_mu.repeat(batch_size, 1),
+                    protein_logvar.repeat(batch_size, 1)
+                ), 0
+            )
+
+            latent_z_omics = torch.unsqueeze(
+                self.reparameterize(
+                    cell_mu.repeat(batch_size, 1),
+                    cell_logvar.repeat(batch_size, 1)
+                ), 0
+            )
+
+            latent_z = torch.mean(torch.cat((latent_z_protein,latent_z_omics),0),0)
+
+        # Generate drugs
+        valid_smiles, valid_nums, _ = self.get_smiles_from_latent(
+            latent_z, remove_invalid=remove_invalid
+        )
+
+        smiles_t = self.smiles_to_numerical(valid_smiles, target='predictor')
+
+        # TODO: combine bowth predictors
+        # Evaluate drugs
+        pred, pred_dict = self.predictor(
+            smiles_t, protein_predictor_tensor.repeat(len(valid_smiles), 1)
+        )
+        pred = np.squeeze(pred.detach().numpy())
+        #self.plot_hist(log_preds, cell_line, epoch, batch_size)
+        # Evaluate drugs
+        pred, pred_dict = self.predictor(
+            smiles_t, gep_t.repeat(len(valid_smiles), 1)
+        )
+        log_preds = self.get_log_molar(np.squeeze(pred.detach().numpy()))
+
+        if return_latent:
+            return valid_smiles, pred, latent_z
+        else:
+            return valid_smiles, pred
+
+
+    def update_reward_fn(self, params):
+        """ Set the reward function
+        
+        Arguments:
+            params {dict} -- Hyperparameter for PaccMann reward function
+
+
+        """
+        super().update_reward_fn(params)
+        self.affinity_weight = params.get('affinity_weight', 1.)
+        self.tox21_weight = params.get('tox21_weight', .5)
+
+        # This is the joint reward function. Each score is normalized to be
+        # inside the range [0, 1].
+        self.reward_fn = (
+            lambda smiles, protein: (
+                self.affinity_weight * self.
+                get_reward_affinity(smiles, protein) + np.
+                array([self.tox21_weight * self.tox21(s) for s in smiles])
+            )
+        )
+
+    def get_reward(self, valid_smiles, protein):
+        """Get the reward
+        
+        Arguments:
+            valid_smiles (list): A list of valid SMILES strings.
+            protein (str): Name of the target protein
+        
+        Returns:
+            np.array: computed reward.
+        """
+        return self.reward_fn(valid_smiles, protein)
+
+    def get_reward_affinity(self, valid_smiles, protein):
+        """
+        Get the reward from affinity predictor
+
+        Args:
+            valid_smiles (list): A list of valid SMILES strings.
+            protein (str): Name of target protein
+
+        Returns:
+            np.array: computed reward (fixed to 1/(1+exp(x))).
+        """
+        # Build up SMILES tensor and GEP tensor
+        smiles_tensor = self.smiles_to_numerical(
+            valid_smiles, target='predictor'
+        )
+
+        # If all SMILES are invalid, no reward is given
+        if len(smiles_tensor) == 0:
+            return 0
+
+        _, protein_tensor = self.protein_to_numerical(
+            protein, encoder_uses_sequence=False
+        )
+
+        pred, pred_dict = self.predictor(
+            smiles_tensor, protein_tensor.repeat(smiles_tensor.shape[0], 1)
+        )
+
+        return np.squeeze(pred.detach().numpy())
+
+    def policy_gradient(self, protein, epoch, batch_size=10):
+        """
+        Implementation of the policy gradient algorithm.
+
+        Args:
+            protein (str): Name of protein to generate drugs.
+            epoch (int): training epoch.
+            batch_size (int): batch size.
+
+        Returns:
+            tuple(float, float): total reward and total loss.
+        """
+        rl_loss = 0
+        self.optimizer.zero_grad()
+
+        # Encode the protein
+        latent_z = self.encode_protein(protein, batch_size)
+
+        # Produce molecules
+        valid_smiles, valid_nums, valid_idx = self.get_smiles_from_latent(
+            latent_z, remove_invalid=True
+        )
+
+        # Get rewards (list, one reward for each valid smiles)
+        rewards = self.get_reward(valid_smiles, protein)
+        reward_tensor = torch.unsqueeze(torch.Tensor(rewards), 1)
+
+        # valid_nums is a list of torch.Tensor, each with varying length,
+        padded_nums = torch.nn.utils.rnn.pad_sequence(valid_nums)
+        num_mols = padded_nums.shape[1]
+
+        self.generator.decoder._update_batch_size(num_mols)
+
+        # Batch processing
+        lrps = 1
+        if self.generator.decoder.latent_dim == 2 * self.encoder.latent_size:
+            lrps = 2
+        hidden = self.generator.decoder.latent_to_hidden(
+            latent_z.repeat(
+                self.generator.decoder.n_layers, 1, lrps
+            )[:, valid_idx, :]
+        )  # yapf: disable
+        stack = self.generator.decoder.init_stack
+
+        # # Compute loss
+        output = torch.Tensor()
+        for p in range(len(padded_nums) - 1):
+            output, hidden, stack = self.generator.decoder(
+                torch.unsqueeze(padded_nums[p], 0), hidden, stack
+            )
+            output = self.generator.decoder.output_layer(output).squeeze()
+
+            log_probs = F.log_softmax(output, dim=1)
+            target_char = torch.unsqueeze(padded_nums[p + 1], 1)
+            rl_loss -= torch.mean(
+                log_probs.gather(1, target_char) * reward_tensor
+            )
+            # discounted_rewards = discounted_rewards * self.gamma
+        summed_reward = torch.mean(torch.Tensor(rewards))
+        rl_loss.backward()
+
+        if self.grad_clipping is not None:
+            torch.nn.utils.clip_grad_norm_(
+                list(self.generator.decoder.parameters()) +
+                list(self.encoder.parameters()), self.grad_clipping
+            )
+        self.optimizer.step()
+        return summed_reward, rl_loss.item()
+
+    def plot_hist(self, log_preds, cell_line, epoch, batch_size):
+        percentage = np.round(
+            100 * (np.sum(log_preds < 0) / len(log_preds)), 1
+        )
+        self.logger.info(f'Percentage of effective compounds = {percentage}')
+        _ = sns.kdeplot(log_preds, shade=True)
+        plt.axvline(x=0)
+        plt.xlabel('Predicted log(micromolar IC50)', weight='bold', size=12)
+        plt.ylabel(
+            f'Density of molecules (n={batch_size})', weight='bold', size=12
+        )
+        cl = cell_line.replace('_', '-')
+        plt.title(
+            f'Predicted IC50 for compounds generated against {cl}',
+            weight='bold'
+        )
+        valid = f'{round((len(log_preds)/batch_size)*100, 1)}% SMILES validity'
+
+        effect = f'{percentage}% compound efficacy.'
+        plt.text(
+            0.05, 0.9, valid, weight='bold', transform=plt.gca().transAxes
+        )
+        plt.text(
+            0.05, 0.8, effect, weight='bold', transform=plt.gca().transAxes
+        )
+        plt.xlim([-7.5, 7.5])
+
+        plt.savefig(
+            self.model_path +
+            f'/results/ic50_dist_ep_{epoch}_cell_{cell_line}.pdf',
+            dpi=400,
+            bbox_inches='tight'
+        )
+        plt.clf()
