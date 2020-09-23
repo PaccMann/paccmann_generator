24b3605008a7870cdd394b7f702983e27d482fa4
Patricia Stoll <tol@zurich.ibm.com>
2020-09-23 16:51:39 +0200

    fix: in protein plot was eff in the file name of aff.

--- paccmann_generator/plot_utils.py
+++ paccmann_generator/plot_utils.py
@@ -150,7 +150,7 @@ def plot_and_compare_proteins(
     plt.savefig(
         os.path.join(
             save_path,
-            f'results/{mode}_{protein}_epoch_{epoch}_eff_{biased_ratio}.pdf'
+            f'results/{mode}_{protein}_epoch_{epoch}_aff_{biased_ratio}.pdf'
         )
     )
     plt.clf()
