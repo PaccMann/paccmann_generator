22291a5998b9e500ca97bdfedf9c92f899a052f8
Patricia Stoll <tol@zurich.ibm.com>
2020-09-30 14:52:44 +0200

    wip:only take data for neuroblastoma.

--- examples/train_paccmann_test.py
+++ examples/train_paccmann_test.py
@@ -21,6 +21,7 @@ sys.path.append('/home/tol/paccmann_affinity')
 from paccmann_affinity.models.predictors import MODEL_FACTORY as MODEL_FACTORY_PROTEIN
 
 from paccmann_generator.reinforce_proteins_omics import ReinforceProteinOmics
+from paccmann_generator import ReinforceOmic
 
 # setup logging
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -35,17 +36,19 @@ protein_model_path = '/mnt/c/Users/PatriciaStoll/paccmann_affinity/trained_model
 ic50_model_path = '../paccmann_003'
 affinity_model_path = '/mnt/c/Users/PatriciaStoll/paccmann_affinity/trained_models/base_affinity'
 omics_data_path = 'data/gdsc_transcriptomics_for_conditional_generation.pkl'
-protein_data_path = '/mnt/c/Users/PatriciaStoll/paccmann_affinity/sars_cov2_data/tape/transformer/avg.csv'
-protein_data_seq_path = '/mnt/c/Users/PatriciaStoll/paccmann_affinity/sars_cov2_data/uniprot_sars_cov2.csv'
+protein_data_path = '/mnt/c/Users/PatriciaStoll/Documents/data/embedding.csv'#paccmann_affinity/sars_cov2_data/tape/transformer/avg.csv'
+protein_data_seq_path = '/mnt/c/Users/PatriciaStoll/Documents/data/uniprot-yourlist M20200922A94466D2655679D1FD8953E075198DA87EBCA61-filtered-rev--.fasta'
 params_path = 'examples/example_params.json'
-model_name = 'test_C_frac_0_5_new_test'
+model_name = 'multitest'
 site = 'lung'
-
+cancertype = 'neuroblastoma'
+cancer_genes = ['RUNX1','CSF1R','MPO','CSF2','IL3','RUNX1T1','HDAC1','HDAC2','SIN3A','NCOR1','CEBPA','PER2','SPI1','CD14','ITGAM','FCGR1A','JUP','PML','RARA','CCNA2','CCNA1','CEBPE','BCL2A1','ZBTB16','MYC','DUSP6','TCF3','PBX1','WNT16','ETV6','ETV7','DEFA1','DEFA3','DEFA4','DEFA5','DEFA6','DEFA1B','ELANE','GZMB','KMT2A','AFF1','CDK9','CCNT1','CCNT2','MLLT1','MLLT3','DOT1L','LMO2','PBX3','RUNX2','SMAD1','KLF3','MEF2C','HOXA9','HOXA10','JMJD1C','HMGA2','KDM6A','SUPT3H','PROM1','FLT3','BMP2K','IGF1R','CDKN1B','CDK14','MEIS1','HOXA11','SIX1','SIX4','EYA1','CDKN2C','HPGD','GRIA3','FUT8','TLX3','TLX1','BCL11B','LDB1','LYL1','HHEX','PTCRA','REL','CCND2','BIRC2','BIRC3','TRAF1','BCL2L1','CD86','CD40','BCL6','IGH','MAF','ITGB7','NSD2','H3-5','H3-3B','H3C4','H3C3','H3C1','H3-3A','H3-4','H3C14','H3C15','H3C13','H3C6','H3C11','H3C8','H3C12','H3C10','H3C2','H3C7','PAX5','PAX8','PPARG','RXRA','RXRB','RXRG','PRCC','TFE3','CDKN1A','TMPRSS2','ERG','PLAU','PLAT','MMP3','MMP9','ZEB1','IL1R2','SPINT1','ETV1','ETV4','ETV5','SLC45A3','ELK4','DDX5','MYCN','MAX','MDM2','PTK2','TP53','BMI1','COMMD3-BMI1','SP1','ZBTB17','NTRK1','NGFR','MEN1','EWSR1','FLI1','IGF1','ID2','TGFBR2','IGFBP3','FEV','ATF1','ARNT2','ATM','MITF','WT1','PDGFA','IL2RB','BAIAP3','TSPAN7','MLF1','NR4A3','TAF15','FUS','DDIT3','CEBPB','IL6','NFKBIZ','NFKB1','RELA','CXCL8','PAX7','PAX3','FOXO1','FLT1','SS18','SSX1','SSX2','SSX2B','NUPR1','ASPSCR1','MET','GADD45A','GADD45B','GADD45G','BAX','BAK1','DDB2','POLK']
 
 disable_rdkit_logging()
 
 params = dict()
 params['site'] = site
+params['cancertype'] = cancertype
 
 
 logger.info(f'Model with name {model_name} starts.')
@@ -54,6 +57,7 @@ logger.info(f'Model with name {model_name} starts.')
 # complement with avg per site
 omics_df = pd.read_pickle(omics_data_path)
 omics_df = add_avg_profile(omics_df)
+omics_df = omics_df[omics_df.histology == cancertype]
 
 # Load protein sequence data
 #if protein_data_path.endswith('.smi'):
@@ -66,9 +70,10 @@ omics_df = add_avg_profile(omics_df)
 #    )
 
 protein_df = pd.read_csv(protein_data_path, index_col=0, header=None, names=[str(x) for x in range(768)]) #'entry_name')
+#print(protein_df.shape)
 protein_df.index = [i.split('|')[2] for i in protein_df.index]
 protein_seq_df = pd.read_csv(protein_data_seq_path, index_col='entry_name')
-protein_df = pd.concat([protein_df, protein_seq_df], axis=1, join='outer')
+#protein_df = pd.concat([protein_df, protein_seq_df], axis=1, join='outer')
 
 with open(params_path) as f:
     params.update(json.load(f))
@@ -242,7 +247,7 @@ gen_mols ,gen_prot, gen_affinity, gen_cell, gen_ic50, modes = [], [], [], [], []
 logger.info('Models restored, start training.')
 
 # choose a validation cell line and protein.
-eval_cell_lines = np.random.choice(test_omics, size = 20, replace=False)
+eval_cell_lines = np.random.choice(test_omics, size = 20, replace=True)
 eval_protein_names = np.random.choice(test_protein.index, size = 20, replace=False)
 
 for epoch in range(1, params['epochs'] + 1):
@@ -371,7 +376,7 @@ for epoch in range(1, params['epochs'] + 1):
         rl_losses,
         rewards,
         params['epochs'],
-        cell_line + ',' + protein_name,
+        #cell_line + ',' + protein_name,
         learner.model_path,
         rolling=5,
         site=site
