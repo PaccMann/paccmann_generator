e685ac7fc11bea9d1809845f55cea1c49ae7531b
Patricia Stoll <tol@zurich.ibm.com>
2020-11-17 08:20:02 +0100

    fix: cell line  needs to be an array.

--- examples/train_paccmann_test.py
+++ examples/train_paccmann_test.py
@@ -315,7 +315,7 @@ logger.info('Models restored, start training.')
 # choose a validation cell line and protein.
 #eval_cell_lines = np.random.choice(test_omics, size = 20, replace=True)
 #eval_cell_lines = [str(i) for i in eval_cell_lines]
-eval_cell_line = test_cell_line
+eval_cell_lines = [test_cell_line]
 eval_protein_names = np.random.choice(test_protein.index, size = 20, replace=False)
 eval_protein_names = [str(i) for i in eval_protein_names]
 
