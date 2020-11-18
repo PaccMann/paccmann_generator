c02b1304c47bc7ca86407bdf4a789bcde6147494
Patricia Stoll <tol@zurich.ibm.com>
2020-11-18 11:38:55 +0100

    feat: also calculate SAS, SCScore and Penalized LogP.

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,9 @@ from paccmann_generator.drug_evaluators.aromatic_ring import AromaticRing
 from paccmann_generator.drug_evaluators.esol import ESOL
 from paccmann_generator.drug_evaluators.molecular_weight import MolecularWeight
 from paccmann_generator.drug_evaluators.qed import QED
+from paccmann_generator.drug_evaluators.sas import SAS
+from paccmann_generator.drug_evaluators.scsore import SCScore
+from paccmann_generator.drug_evaluators.penalized_logp import PenalizedLogP
 model='liver_concat_allValid_SNU-423_combined'
 mols = pd.read_csv('biased_models/'+model+'/results/generated_per_epoch.csv').drop(columns=['Unnamed: 0'])
 loss = pd.read_csv('biased_models/'+model+'/results/loss_and_more.csv').drop(columns=['Unnamed: 0'])
@@ -27,12 +30,18 @@ arom = AromaticRing()
 esol = ESOL()
 mol_weight = MolecularWeight()
 qed = QED()
+sas = SAS()
+scscore = SCScore()
+penalized_logp = PenalizedLogP()
 
 mols['C_frac'] = [np.nan]*mols.shape[0]
 mols['aromatic'] = [np.nan]*mols.shape[0]
 mols['esol'] = [np.nan]*mols.shape[0]
 mols['mol_weight'] = [np.nan]*mols.shape[0]
 mols['qed'] = [np.nan]*mols.shape[0]
+mols['sas'] = [np.nan]*mols.shape[0]
+mols['SCScore'] = [np.nan]*mols.shape[0]
+mols['penalized_logP'] = [np.nan]*mols.shape[0]
 for idx, s in enumerate(mols['SMILES']):
     mol = Chem.MolFromSmiles(s)
     if(mol): smile = Chem.MolToSmiles(mol)
@@ -43,12 +52,18 @@ for idx, s in enumerate(mols['SMILES']):
         mols.loc[idx, 'esol'] = esol(s)
         mols.loc[idx, 'mol_weight'] = mol_weight(s)
         mols.loc[idx, 'qed'] = qed(s)
+        mols.loc[idx, 'sas'] = sas(s)
+        mols.loc[idx, 'SCScore'] = scscore(s)
+        mols.loc[idx, 'penalized_logP'] = penalized_logp(s)
     else:
         mols.loc[idx, 'C_frac'] = np.nan
         mols.loc[idx, 'aromatic'] = np.nan
         mols.loc[idx, 'esol'] = np.nan
         mols.loc[idx, 'mol_weight'] = np.nan
         mols.loc[idx, 'qed'] = np.nan
+        mols.loc[idx, 'sas'] = np.nan
+        mols.loc[idx, 'SCScore'] = np.nan
+        mols.loc[idx, 'penalized_logP'] = np.nan
 print("describe",mols.describe())
 mols = mols.groupby(['epoch']).mean()
 print(mols)
