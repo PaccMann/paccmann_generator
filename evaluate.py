21384e3c7f8a16d67412c05fb41ddc0f3843ae3f
Patricia Stoll <tol@zurich.ibm.com>
2020-11-18 09:45:22 +0100

    feat: calculate if aromaticcity there.

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
+from rdkit import Chem
 from paccmann_generator.drug_evaluators.aromatic_ring import AromaticRing
+#from paccmann_generator,drug_evaluators.esol import ESOL
 model='liver_concat_allValid_SNU-423_combined'
-mols = pd.read_csv('biased_models/'+model+'/results/generated.csv')
-loss = pd.read_csv('biased_models/'+model+'/results/loss_reward_evolution.csv')
+mols = pd.read_csv('biased_models/'+model+'/results/generated_per_epoch.csv')
+loss = pd.read_csv('biased_models/'+model+'/results/loss_and_more.csv')
 
 def get_C_fraction(smiles):
         """get the fraction of C atoms in the molecule
@@ -21,23 +23,24 @@ def get_C_fraction(smiles):
         return C
 arom = AromaticRing()
 
-mols = mols.loc[:500].drop(columns=['Unnamed: 0'])
+mols = mols.drop(columns=['Unnamed: 0'])
 mols['C_frac'] = [np.nan]*mols.shape[0]
 mols['aromatic'] = [np.nan]*mols.shape[0]
 for idx, s in enumerate(mols['SMILES']):
-    mols.loc[idx, 'C_frac'] = get_C_fraction(s)
-    mols.loc[idx, 'aromatic'] = arom(s)
-
-loss = loss.loc[:50]
+    mol = Chem.MolFromSmiles(s)
+    if(mol): smile = Chem.MolToSmiles(mol)
+    else: smile=None
+    if(smile):
+        mols.loc[idx, 'C_frac'] = get_C_fraction(s)
+        mols.loc[idx, 'aromatic'] = arom(s)
+    else:
+        mols.loc[idx, 'C_frac'] = np.nan
+        mols.loc[idx, 'aromatic'] = np.nan
+print("mols",mols.describe())
 mols = mols.groupby(['epoch']).mean()
 print(mols)
 #loss['C_frac'] = [np.nan]*loss.shape[0]
 #loss['aromatic'] = [np.nan]*loss.shape[0]
 loss = loss.join(mols, on='epoch') 
 print(loss)
-1/0
-
-for idx, s in enumerate(mols['SMILES']):
-    loss.loc[idx, 'C_frac'] = get_C_fraction(s)
-
-print(mols.head(), loss.head(), loss.columns, loss.shape, mols.shape)
+loss.to_csv('biased_models/'+model+'/results/loss_and_metrics.csv')
