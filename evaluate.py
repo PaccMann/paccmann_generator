ebf518eeb792038a352469a4bb82da84d17f2333
Patricia Stoll <tol@zurich.ibm.com>
2020-11-18 10:22:19 +0100

    feat: calculate molecular weight.

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,7 @@ import numpy as np
 from rdkit import Chem
 from paccmann_generator.drug_evaluators.aromatic_ring import AromaticRing
 from paccmann_generator.drug_evaluators.esol import ESOL
+from paccmann_generator.drug_evaluators.molecular_weight import MolecularWeight
 model='liver_concat_allValid_SNU-423_combined'
 mols = pd.read_csv('biased_models/'+model+'/results/generated_per_epoch.csv')
 loss = pd.read_csv('biased_models/'+model+'/results/loss_and_more.csv')
@@ -23,11 +24,13 @@ def get_C_fraction(smiles):
         return C
 arom = AromaticRing()
 esol = ESOL()
+mol_weight = MolecularWeight()
 
 mols = mols.drop(columns=['Unnamed: 0'])
 mols['C_frac'] = [np.nan]*mols.shape[0]
 mols['aromatic'] = [np.nan]*mols.shape[0]
 mols['esol'] = [np.nan]*mols.shape[0]
+mols['mol_weight'] = [np.nan]*mols.shape[0]
 for idx, s in enumerate(mols['SMILES']):
     mol = Chem.MolFromSmiles(s)
     if(mol): smile = Chem.MolToSmiles(mol)
@@ -36,11 +39,13 @@ for idx, s in enumerate(mols['SMILES']):
         mols.loc[idx, 'C_frac'] = get_C_fraction(s)
         mols.loc[idx, 'aromatic'] = arom(s)
         mols.loc[idx, 'esol'] = esol(s)
+        mols.loc[idx, 'mol_weight'] = mol_weight(s)
     else:
         mols.loc[idx, 'C_frac'] = np.nan
         mols.loc[idx, 'aromatic'] = np.nan
         mols.loc[idx, 'esol'] = np.nan
-print("mols",mols.describe())
+        mols.loc[idx, 'mol_weight'] = np.nan
+print("describe",mols.describe())
 mols = mols.groupby(['epoch']).mean()
 print(mols)
 #loss['C_frac'] = [np.nan]*loss.shape[0]
