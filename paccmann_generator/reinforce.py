5623fddcc5ebd6a079a05b3b39ec9da3143f1770
Patricia Stoll <tol@zurich.ibm.com>
2020-12-11 11:11:45 +0100

    fix: handling of generating only empty smiles.

--- paccmann_generator/reinforce.py
+++ paccmann_generator/reinforce.py
@@ -196,16 +196,18 @@ class Reinforce(object):
         ]
         if self.remove_invalid:
             imgs = [Chem.MolFromSmiles(s, sanitize=True) for s in smiles]
-            valid_idxs = [ind for ind in range(len(imgs)) if (imgs[ind] is not None) and (imgs[ind])]
+            valid_idxs = [ind for ind in range(len(imgs)) if (imgs[ind] is not None) and (Chem.MolToSmiles(imgs[ind]))]
         else:
             imgs = [Chem.MolFromSmiles(s, sanitize=False) for s in smiles]
             valid_idxs = [ind for ind in range(len(imgs)) if (imgs[ind] is not None)]
         # valid_idxs = [ind for ind, img in enumerate(imgs) if img is not None]
 
-        
-        smiles, nums = zip(*[
-           (smiles[ind], numericals[ind]) for ind in valid_idxs
-        ])
+        if not valid_idxs:
+            smiles, nums = [], []
+        else:
+            smiles, nums = zip(*[
+            (smiles[ind], numericals[ind]) for ind in valid_idxs
+            ])
         # nums = [
         #     numericals[ind] for ind in range(len(numericals))
         #     if not (self.remove_invalid and imgs[ind] is None)
