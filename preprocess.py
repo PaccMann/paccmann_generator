a046389fa2993d356a986eee278688207eec44b2
Patricia Stoll <tol@zurich.ibm.com>
2020-12-14 10:19:56 +0100

    wip: translate to gene names.

--- preprocess.py
+++ preprocess.py
@@ -5,10 +5,16 @@ from sklearn.preprocessing import StandardScaler
 from paccmann_generator.utils import add_avg_profile
 from scipy.stats import wilcoxon
 from scipy.spatial.distance import jensenshannon
+import sys
+sys.path.append('/home/tol/interact')
+from interact.uniprot import dict_from_uniprot
+
+gene_names = dict_from_uniprot(key_name='HGNC')
 
 scaler = StandardScaler()
 data2 = pd.read_pickle('data/gdsc_transcriptomics_for_conditional_generation.pkl')
 data2 = add_avg_profile(data2)
+print(data2.head(3))
 df = pd.DataFrame(index=data2['cell_line'], columns=range(2128))
 for cell in data2['cell_line']:
     df.loc[cell,:] = data2[data2['cell_line']==cell]['gene_expression'].values[0].astype(float)
@@ -32,8 +38,9 @@ print(scaled_data.std(axis = 0)) # should be 1
 # how many cell lines overlap:
 print(df.index)
 print(scaled_data.columns)
-genes = [str(i).lower() for i in df.index]
-genes2 = [i.lower() for i in scaled_data.columns]
+genes = [str(i) for i in df.index]
+genes2 = [i for i in scaled_data.columns]
+print("overlap dict:", [gene_names[gene] for gene in genes2 if gene in gene_names])
 overlap = [gene for gene in genes if gene in genes2]
 print("overlapping on cell lines:", overlap)
 
