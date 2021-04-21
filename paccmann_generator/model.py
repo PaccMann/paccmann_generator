9d0fcf35299b65a82dde3d7d748f3b9d455b2ec0
Patricia Stoll <tol@zurich.ibm.com>
2021-04-21 13:56:37 +0200

    refactor: use parameters instead of hardcoded stuff.

--- paccmann_generator/model.py
+++ paccmann_generator/model.py
@@ -21,14 +21,12 @@ from paccmann_generator.utils import disable_rdkit_logging
 from paccmann_predictor.models import MODEL_FACTORY as MODEL_FACTORY_OMICS
 from paccmann_sets.models.sets_autoencoder import SetsAE
 import sys
-#sys.path.append('/dataP/tol/paccmann_affinity')
 sys.path.append('/home/tol/paccmann_affinity')
 from paccmann_affinity.models.predictors import MODEL_FACTORY as MODEL_FACTORY_PROTEIN
 
 from paccmann_generator.reinforce_proteins_omics import ReinforceProteinOmics
 from paccmann_generator import ReinforceOmic
 from paccmann_generator.reinforce_proteins import ReinforceProtein
-from files import *
 
 class Model:
     
@@ -46,34 +44,34 @@ class Model:
     def create_combined_model(self, params, params_o, params_p, omics_df, protein_df, logger, model_folder_name):
         # Load languages
         generator_smiles_language = SMILESLanguage.load(
-            os.path.join(mol_model_path, 'selfies_language.pkl')
+            os.path.join(params['mol_model_path'], 'selfies_language.pkl')
         )
 
         #load predictors
-        with open(os.path.join(ic50_model_path, 'model_params.json')) as f:
+        with open(os.path.join(params['ic50_model_path'], 'model_params.json')) as f:
             paccmann_params = json.load(f)
 
         paccmann_predictor = MODEL_FACTORY_OMICS['mca'](paccmann_params)
         paccmann_predictor.load(
             os.path.join(
-                ic50_model_path,
+                params['ic50_model_path'],
                 f"weights/best_{params.get('ic50_metric', 'mse')}_mca.pt"
             ),
             map_location=get_device()
         )
         paccmann_predictor.eval()
         paccmann_smiles_language = SMILESLanguage.load(
-            os.path.join(ic50_model_path, 'smiles_language.pkl')
+            os.path.join(params['ic50_model_path'], 'smiles_language.pkl')
         )
         paccmann_predictor._associate_language(paccmann_smiles_language)
 
-        with open(os.path.join(affinity_model_path, 'model_params.json')) as f:
+        with open(os.path.join(params['affinity_model_path'], 'model_params.json')) as f:
             protein_pred_params = json.load(f)
 
         protein_predictor = MODEL_FACTORY_PROTEIN['bimodal_mca'](protein_pred_params)
         protein_predictor.load(
             os.path.join(
-                affinity_model_path,
+                params['affinity_model_path'],
                 f"weights/best_{params.get('p_metric', 'ROC-AUC')}_bimodal_mca.pt"
             ),
             map_location=get_device()
@@ -81,10 +79,10 @@ class Model:
         protein_predictor.eval()
 
         affinity_smiles_language = SMILESLanguage.load(
-            os.path.join(affinity_model_path, 'smiles_language.pkl')
+            os.path.join(params['affinity_model_path'], 'smiles_language.pkl')
         )
         affinity_protein_language = ProteinLanguage.load(
-            os.path.join(affinity_model_path, 'protein_language.pkl')
+            os.path.join(params['affinity_model_path'], 'protein_language.pkl')
         )
         protein_predictor._associate_language(affinity_smiles_language)
         protein_predictor._associate_language(affinity_protein_language)
@@ -92,14 +90,14 @@ class Model:
         #############################################
         # Create a fresh model that will be optimized
         # Restore SMILES Model
-        with open(os.path.join(mol_model_path, 'model_params.json')) as f:
+        with open(os.path.join(params['mol_model_path'], 'model_params.json')) as f:
             mol_params = json.load(f)
         gru_encoder_rl = StackGRUEncoder(mol_params)
         gru_decoder_rl = StackGRUDecoder(mol_params)
         generator_rl = TeacherVAE(gru_encoder_rl, gru_decoder_rl)
         generator_rl.load(
             os.path.join(
-                mol_model_path, f"weights/best_{params.get('metric', 'rec')}.pt"
+                params['mol_model_path'], f"weights/best_{params.get('metric', 'rec')}.pt"
             ),
             map_location=get_device()
         )
@@ -107,12 +105,12 @@ class Model:
         #generator_rl._associate_language(generator_smiles_language)
 
         # Restore omics model
-        with open(os.path.join(omics_model_path, 'model_params.json')) as f:
+        with open(os.path.join(params['omics_model_path'], 'model_params.json')) as f:
             cell_params = json.load(f)
         cell_encoder_rl = ENCODER_FACTORY['dense'](cell_params)
         cell_encoder_rl.load(
             os.path.join(
-                omics_model_path,
+                params['omics_model_path'],
                 f"weights/best_{params.get('metric', 'both')}_encoder.pt"
             ),
             map_location=get_device()
@@ -120,12 +118,12 @@ class Model:
         cell_encoder_rl.eval()
 
         # Restore protein model
-        with open(os.path.join(protein_model_path, 'model_params.json')) as f:
+        with open(os.path.join(params['protein_model_path'], 'model_params.json')) as f:
             protein_params = json.load(f)
         protein_encoder_rl = ENCODER_FACTORY['dense'](protein_params)
         protein_encoder_rl.load(
             os.path.join(
-                protein_model_path,
+                params['protein_model_path'],
                 f"weights/best_{params.get('metric', 'both')}_encoder.pt"
             ),
             map_location=get_device()
@@ -133,16 +131,16 @@ class Model:
         protein_encoder_rl.eval()
 
         if (model_folder_name is None):
-            model_folder_name = site + '_' + self.type + '_' + model_name + '_combined'
+            model_folder_name = site + '_' + self.type + '_' + params['model_name'] + '_combined'
         if self.type =='set':
             adapted_keys = ['encoder.lstm.weights_x', 'encoder.lstm.weights_h', 'encoder.lstm.weights_c', 'encoder.lstm.bias', 'encoder.memory_mapping.weight', 'encoder.memory_mapping.bias', 'decoder.lstm.weights_x', 'decoder.lstm.weights_h', 'decoder.lstm.weights_c', 'decoder.lstm.bias', 'decoder.output_layer.weight', 'decoder.output_layer.bias', 'decoder.prob_layer.0.weight', 'decoder.prob_layer.0.bias']
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
             #TO-DO: get the right encoder
-            with open(os.path.join(set_encoder_path, 'train_params.json')) as f:
+            with open(os.path.join(params['set_encoder_path'], 'train_params.json')) as f:
                 encoder_params = json.load(f)
             #rename model keys
             new_state_dict = {}
-            checkpoint = torch.load(set_encoder_path+'/setsae', map_location=device)
+            checkpoint = torch.load(params['set_encoder_path']+'/setsae', map_location=device)
             state_dict = checkpoint["model_state_dict"]
             for i, (k,v) in enumerate(state_dict.items()):
                 new_state_dict[adapted_keys[i]] = v
@@ -154,12 +152,12 @@ class Model:
             combined = ReinforceProteinOmics( 
                 generator_rl, protein_encoder_rl, cell_encoder_rl, protein_predictor, \
                 paccmann_predictor, protein_df, omics_df, params, params_o, params_p, generator_smiles_language, \
-                model_folder_name, logger, remove_invalid, ensemble_type=self.type, set_encoder=encoder_rl)
+                model_folder_name, logger, params['remove_invalid'], ensemble_type=self.type, set_encoder=encoder_rl)
         else:
             combined = ReinforceProteinOmics(
                 generator_rl, protein_encoder_rl, cell_encoder_rl, \
                 protein_predictor, paccmann_predictor, protein_df, omics_df, params, params_o, params_p, \
-                generator_smiles_language, model_folder_name, logger, remove_invalid, ensemble_type=self.type
+                generator_smiles_language, model_folder_name, logger, params['remove_invalid'], ensemble_type=self.type
             )
         return combined
           
@@ -271,7 +269,7 @@ class Model:
                 'IC50': self.gen_ic50
             }
             plot_and_compare(
-                unbiased_predsO, predsO, site, cell_line, epoch, self.model.model_path,
+                unbiased_predsO, predsO, param['site'], cell_line, epoch, self.model.model_path,
                 mode, param
             )
         else:
@@ -293,7 +291,7 @@ class Model:
                 'IC50': self.gen_ic50
             }
             plot_and_compare(
-                unbiased_predsO, predsO, site, cell_line, epoch, self.model.model_path,
+                unbiased_predsO, predsO, param['site'], cell_line, epoch, self.model.model_path,
                 mode, param
             )
             plot_and_compare_proteins(
